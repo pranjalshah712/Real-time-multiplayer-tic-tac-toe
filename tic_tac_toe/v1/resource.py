577240f472ebf91d74d552471350d4dbb0abb5fa
pranjal_shah <pranjalshah712@gmail.com>
2024-09-20 10:01:25 +0530

    add comments

--- tic_tac_toe/v1/resource.py
+++ tic_tac_toe/v1/resource.py
@@ -6,185 +6,153 @@ import random
 import json
 from string import ascii_uppercase
 
+# List to hold room codes
 rooms = []
 
 
+# Function to generate a unique room code
 def generate_room_code():
-    while True:
-        code = ""
-        for _ in range(5):
-            code += random.choice(ascii_uppercase)
-        if code not in rooms:
-            rooms.append(code)
-            break
-    return code
+	while True:
+		code = ""
+		# Generate a 5-letter code using uppercase letters
+		for _ in range(5):
+			code += random.choice(ascii_uppercase)
+		# Ensure the code is unique
+		if code not in rooms:
+			rooms.append(code)  # Add to rooms if unique
+			break
+	return code
 
 
+# Resource for the home page
 class Home(Resource):
 
-    def get(self):
-        return make_response(render_template('home.html'))
+	def get(self):
+		# Render the home template
+		return make_response(render_template('home.html'))
 
-    def post(self):
-        name = request.form.get('name')
-        room_code = request.form.get('room_code')
-        join = request.form.get('join', False)
-        create = request.form.get('create', False)
+	def post(self):
+		name = request.form.get('name')  # Get the username from the form
+		room_code = request.form.get('room_code')  # Get the room code
+		join = request.form.get('join', False)  # Check if user wants to join a room
+		create = request.form.get('create', False)  # Check if user wants to create a room
 
-        if join is False:
-            new_room_code = generate_room_code()
-            return redirect(url_for('game', room_code=new_room_code, username=name, player=1))
+		if join is False:
+			# If not joining, create a new room
+			new_room_code = generate_room_code()
+			return redirect(url_for('game', room_code=new_room_code, username=name, player=1))
 
-        if create is False and room_code == '':
-            return make_response(render_template('home.html', error=True, username=name))
-        else:
-            try:
-                rooms.remove(room_code)
-                return redirect(url_for('game', room_code=room_code, username=name, player=2))
-            except:
-                return make_response(render_template('home.html', error_max=True, username=name))
+		# If creating a room, but no room code is provided
+		if create is False and room_code == '':
+			return make_response(render_template('home.html', error=True, username=name))
+		else:
+			try:
+				# Try to remove the room code if it exists (indicates joining)
+				rooms.remove(room_code)
+				return redirect(url_for('game', room_code=room_code, username=name, player=2))
+			except:
+				# If room code does not exist, show error
+				return make_response(render_template('home.html', error_max=True, username=name))
 
 
+# Resource for the game page
 class Game(Resource):
-    def get(self, room_code, username, player):
-        return make_response(render_template('game.html', room_code=room_code, username=username, player=player))
+	def get(self, room_code, username, player):
+		# Render the game template with room code and player information
+		return make_response(render_template('game.html', room_code=room_code, username=username, player=player))
 
 
+# Set the square numbers for a user in Redis
 def set_square_numbers(user, square_numbers):
-    redis_ob.set(user, json.dumps(square_numbers))
+	redis_ob.set(user, json.dumps(square_numbers))
 
 
+# Get square numbers for a user and append the new move
 def get_square_numbers(user, move):
-    square_numbers = json.loads(redis_ob.get(user))
-    square_numbers.append(move['square_number'])
-    square_numbers.sort()
-    return square_numbers
+	square_numbers = json.loads(redis_ob.get(user))  # Retrieve current square numbers
+	square_numbers.append(move['square_number'])  # Add the new move
+	square_numbers.sort()  # Sort the moves
+	return square_numbers
 
 
+# Namespace for handling game events
 class GameStart(Namespace):
 
-    def __init__(self, *args, **kwargs):
-        super().__init__(*args, **kwargs)
-        self.current_user = []
-        self.user1 = ''
-        self.user2 = ''
-        self.loser = ''
-        self.flag = True
-        self.swap_user = ['0']
-        self.count = 0
-        self.win_conditions = [
-            [1, 2, 3], [1, 4, 7], [4, 5, 6], [2, 5, 8],
-            [7, 8, 9], [3, 6, 9], [1, 5, 9], [3, 5, 7]
-        ]
+	def __init__(self, *args, **kwargs):
+		super().__init__(*args, **kwargs)
+		self.current_user = []  # Current player's moves
+		self.user1 = ''  # Player 1 username
+		self.user2 = ''  # Player 2 username
+		self.loser = ''  # Track the loser
+		self.flag = True  # Flag to check if the game is ongoing
+		self.swap_user = ['0']  # Track turns
+		self.count = 0  # Count of moves
+		# Winning combinations for Tic Tac Toe
+		self.win_conditions = [
+			[1, 2, 3], [1, 4, 7], [4, 5, 6], [2, 5, 8],
+			[7, 8, 9], [3, 6, 9], [1, 5, 9], [3, 5, 7]
+		]
 
-    def on_connect(self):
-        set_square_numbers('user1', [])
-        set_square_numbers('user2', [])
-        self.count = 0
-        # self.user1.clear()
-        # self.user2.clear()
-        self.flag = True
-        self.swap_user = ['0']
-        print('user_connect')
+	def on_connect(self):
+		# Initialize game state when a user connects
+		set_square_numbers('user1', [])  # Reset Player 1's moves
+		set_square_numbers('user2', [])  # Reset Player 2's moves
+		self.count = 0  # Reset move count
+		self.flag = True  # Set game ongoing flag
+		self.swap_user = ['0']  # Reset turn tracking
+		print('user_connect')
 
-    def on_move(self, move):
-        if move['player'] != self.swap_user[-1]:
+	def on_move(self, move):
+		# Handle a player's move
+		if move['player'] != self.swap_user[-1]:  # Check if it's the correct player's turn
 
-            if self.user1 == '':
-                self.user1 = move['username']
-            if self.user1 != move['username'] and self.user2 == '':
-                self.user2 = move['username']
-            loser = '2' if move['player'] == '1' else '1'
-            if self.user1 == move['username']:
-                self.loser = self.user2
-            else:
-                self.loser = self.user1
+			# Assign usernames to player variables
+			if self.user1 == '':
+				self.user1 = move['username']
+			if self.user1 != move['username'] and self.user2 == '':
+				self.user2 = move['username']
 
-            if move['player'] == str(1):
-                square_numbers = get_square_numbers('user1', move)
-                print(square_numbers)
-                set_square_numbers('user1', square_numbers)
-                self.current_user = square_numbers
-                # self.user1.append(move['square_number'])
-                # self.user1.sort()
-                # self.current_user = self.user1
+			loser = '2' if move['player'] == '1' else '1'  # Determine loser based on player turn
+			self.loser = self.user2 if self.user1 == move['username'] else self.user1
 
-            else:
-                square_numbers = get_square_numbers('user2', move)
-                print(square_numbers)
-                set_square_numbers('user2', square_numbers)
-                self.current_user = square_numbers
-                # self.user2.append(move['square_number'])
-                # self.user2.sort()
-                # self.current_user = self.user1
+			# Update square numbers based on which player made the move
+			if move['player'] == str(1):
+				square_numbers = get_square_numbers('user1', move)
+				print(square_numbers)
+				set_square_numbers('user1', square_numbers)
+				self.current_user = square_numbers
+			else:
+				square_numbers = get_square_numbers('user2', move)
+				print(square_numbers)
+				set_square_numbers('user2', square_numbers)
+				self.current_user = square_numbers
 
-            if len(self.current_user) > 2:
+			# Check for a win condition
+			if len(self.current_user) > 2:
+				for i in range(len(self.current_user) - 2):
+					for j in range(i + 1, len(self.current_user) - 1):
+						if [self.current_user[i], self.current_user[j], self.current_user[j + 1]] in self.win_conditions:
+							# Emit events for win and loss
+							emit('current_move', move, broadcast=True)
+							emit('win', {'player': move['player'],
+							             'square': [self.current_user[i], self.current_user[j],
+							                        self.current_user[j + 1]],
+							             'username': move['username']}, broadcast=True)
 
-                for i in range(len(self.current_user) - 2):
-                    for j in range(i + 1, len(self.current_user) - 1):
-                        if [self.current_user[i], self.current_user[j], self.current_user[j + 1]] in self.win_conditions:
-                            emit('current_move', move, broadcast=True)
-                            emit('win', {'player': move['player'],
-                                         'square': [self.current_user[i], self.current_user[j],
-                                                    self.current_user[j + 1]],
-                                         'username': move['username']}, broadcast=True)
+							emit('loss', {'player': loser, 'username': self.loser}, broadcast=True)
+							self.flag = False  # Game has ended
+							break
 
-                            emit('loss', {'player': loser, 'username': self.loser}, broadcast=True)
-                            self.flag = False
-                            break
+			# If no win, continue the game
+			if self.flag is True:
+				self.count += 1  # Increment move count
+				if self.count == 9:  # Check for a draw
+					print(self.count)
+					emit('end_move', move, broadcast=True)  # Emit draw event
+				else:
+					print(self.count)
+					emit('current_move', move, broadcast=True)  # Emit current move event
+			else:
+				emit('current_move', move, broadcast=True)  # Emit the move even after a win
 
-            if self.flag is True:
-                self.count += 1
-                if self.count == 9:
-                    print(self.count)
-                    emit('end_move', move, broadcast=True)
-                else:
-                    print(self.count)
-                    emit('current_move', move, broadcast=True)
-            else:
-                emit('current_move', move, broadcast=True)
-
-            self.swap_user.append(move['player'])
-
-            # if len(self.user1) > 2:
-            #     try:
-            #         for i in range(len(self.user1) - 2):
-            #             if flag is False:
-            #                 break
-            #             for j in range(i + 1, len(self.user1)):
-            #                 if [self.user1[i], self.user1[j], self.user1[j + 1]] in self.win_conditions:
-            #                     emit('current_move', move, broadcast=True)
-            #                     emit('win', {'player': move['player'],
-            #                                  'square': [self.user1[i], self.user1[j], self.user1[j + 1]],
-            #                                  'username': move['username']}, broadcast=True)
-            #                     flag = False
-            #                     break
-            #     except IndexError as e:
-            #         pass
-            # if len(self.user2) > 2:
-            #     try:
-            #         for i in range(len(self.user2) - 2):
-            #             if flag is False:
-            #                 break
-            #             for j in range(i + 1, len(self.user2)):
-            #                 if [self.user2[i], self.user2[j], self.user2[j + 1]] in self.win_conditions:
-            #                     emit('current_move', move, broadcast=True)
-            #
-            # emit('win', {'player': move['player'], 'square': [self.user2[i], self.user2[j], self.user2[j + 1]],
-            # 'username': move['username']}, broadcast=True) flag = False break except IndexError as e: pass
-
-            # if len(self.current_user) > 2:
-            #     # try:
-            #     for i in range(len(self.current_user) - 2):
-            #         # if self.flag is False:
-            #         #     break
-            #         for j in range(i + 1, len(self.current_user) - 1):
-            #             if [self.current_user[i], self.current_user[j], self.current_user[j + 1]] in self.win_conditions:
-            #                 emit('current_move', move, broadcast=True)
-            #                 emit('win', {'player': move['player'],
-            #                             'square': [self.current_user[i], self.current_user[j], self.current_user[j + 1]],
-            #                             'username': move['username']}, broadcast=True)
-            #                 self.flag = False
-            #                 break
-            #     # except IndexError as e:
-            #     #     print(e)
+			self.swap_user.append(move['player'])  # Switch turns
